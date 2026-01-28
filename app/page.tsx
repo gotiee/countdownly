ca3898198abc10fc0c7afd325a741f1016ca1eca
Gauthier <gauthier.solignac@jetdev.fr>
2026-01-28 15:36:09 +0100

    :sparkles: Adds link to admin page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { db } from "@/db/drizzle";
 import { countdown } from "@/db/auth-schema";
 import { desc, eq } from "drizzle-orm";
 import { PublicCountdownList } from "@/components/custom/countdown/public-countdown-list";
+import Link from "next/link";
 
 export default async function PublicCountdownsPage() {
   const publicCountdowns = await db
@@ -12,6 +13,12 @@ export default async function PublicCountdownsPage() {
 
   return (
     <div style={{ maxWidth: "800px", margin: "0 auto", padding: "2rem" }}>
+      <Link
+        href="/admin"
+        className="absolute top-[1rem] right-[1rem] border p-2 rounded-md bg-gray-200 hover:bg-gray-300 transition text-xs"
+      >
+        Edit My Countdowns
+      </Link>
       <h1
         style={{
           fontSize: "1.5rem",
